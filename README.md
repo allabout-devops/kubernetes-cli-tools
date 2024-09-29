000a1aa677d0cc9c64b044c52d90b458bcc76a3d
AllAboutDevOps <allaboutdevops@AllAboutDevOps.local>
2024-09-29 00:02:22 -0700

    Added kns function

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
+To use these functions copy  required function from kubernetes.sh file in ~/.bashrc or ~/.zshrc file 
+
 Use kgp to list pods with filters in current namespace or specific namespace using flag -n <namespace_name>
 
 Example 
 ##
-        kgp test 
-        kgp -n test
-        kgp nginx test -n test
+        kgp test                # List all pod in current namespace and filter with test word
+        kgp -n test             # List all pods in test namespace
+        kgp nginx test -n test  # List all pods in test namespace and filter with words test or nginx 
+
+Use kns to play with namesapces 
+
+Example 
+
+##
+        kns         # List all namespaces with index number 
+        kns test    # Switch to test namespace
 
--- kubernetes.sh
+++ kubernetes.sh
@@ -1,3 +1,22 @@
+COLOR_RESET="\033[0m"
+COLOR_BLACK="\033[0;30m"
+COLOR_RED="\033[0;31m"
+COLOR_GREEN="\033[0;32m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_BLUE="\033[0;34m"
+COLOR_MAGENTA="\033[0;35m"
+COLOR_CYAN="\033[0;36m"
+COLOR_WHITE="\033[0;37m"
+COLOR_BOLD_BLACK="\033[1;30m"
+COLOR_BOLD_RED="\033[1;31m"
+COLOR_BOLD_GREEN="\033[1;32m"
+COLOR_BOLD_YELLOW="\033[1;33m"
+COLOR_BOLD_BLUE="\033[1;34m"
+COLOR_BOLD_MAGENTA="\033[1;35m"
+COLOR_BOLD_CYAN="\033[1;36m"
+COLOR_BOLD_WHITE="\033[1;37m"
+
+
 alias k=kubectl
 
 # List all pods in current namespace or specify namespace with flag -n and filter with multiple argument 
@@ -27,5 +46,34 @@ kgp() {
     kubectl get pods | grep -E "$grep_pattern"
   fi
 }
+
+kns() {
+
+  # Get the list of namespaces
+  local namespaces=($(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}'))
+  local current_ns=$(kubectl config view --minify --output 'jsonpath={..namespace}')
+  current_ns=${current_ns:-"default"}  # Default to "default" if no namespace is set
+
+  if [ "$1" ]; then
+    # Check if the argument is a valid index
+    if [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -lt "${#namespaces[@]}" ]; then
+      kubectl config set-context --current --namespace="${namespaces[$1]}"
+      echo -e "${COLOR_GREEN}Switched to namespace: ${namespaces[$1]}${COLOR_RESET}"
+    else
+      echo -e "${COLOR_YELLOW}Invalid index. Please provide a number between 0 and $((${#namespaces[@]} - 1)).${COLOR_RESET}"
+    fi
+  else
+    echo -e "${COLOR_YELLOW}Available namespaces:${COLOR_RESET}"
+    for i in "${!namespaces[@]}"; do
+      if [ "${namespaces[$i]}" == "$current_ns" ]; then
+        echo -e "$i: ${COLOR_BOLD_BLUE}${namespaces[$i]}${COLOR_RESET}"  # Highlight current namespace
+      else
+        echo "$i: ${namespaces[$i]}"
+      fi
+    done
+  fi
+}
+
+
 
 
